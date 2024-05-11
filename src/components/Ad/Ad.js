da7a75d5f34565a1baf4bc5f5a2506e00d7581fd
SancheSLozovoy <alex.lozovoy05@bk.ru>
2024-05-11 18:09:54 +0300

    end

--- src/components/Ad/Ad.js
+++ src/components/Ad/Ad.js
@@ -1,26 +1,40 @@
-// Ad.js
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Ad.css';
 
-function Ad({ ad, onDelete, user }) {
+function Ad({ ad, onDelete, currentUser, removeFromFavorites }) {
     const [userName, setUserName] = useState('');
+    const [isCurrentUserAd, setIsCurrentUserAd] = useState(false);
+    const [isFavorite, setIsFavorite] = useState(false);
+    const [favoriteId, setFavoriteId] = useState(null);
 
     useEffect(() => {
-        if (user) {
-            setUserName(user.Name);
-        } else if (ad.userId) {
+        if (ad.userId) {
             axios.get(`https://fe4f5b2b7285d6c0.mokky.dev/users/${ad.userId}`)
                 .then(response => {
                     const userData = response.data;
                     setUserName(userData.Name);
+                    setIsCurrentUserAd(currentUser && ad.userId === currentUser.id);
                 })
                 .catch(error => {
                     console.error('Ошибка при получении информации о пользователе:', error);
                 });
         }
-    }, [ad.userId, user]);
+    }, [ad.userId, currentUser]);
+
+    useEffect(() => {
+        if (currentUser) {
+            axios.get(`https://fe4f5b2b7285d6c0.mokky.dev/favorites?userId=${currentUser.id}&adId=${ad.id}`)
+                .then(response => {
+                    const favorite = response.data[0];
+                    setIsFavorite(favorite !== undefined);
+                    setFavoriteId(favorite ? favorite.id : null);
+                })
+                .catch(error => {
+                    console.error('Ошибка при проверке избранного:', error);
+                });
+        }
+    }, [currentUser, ad.id]);
 
     const handleDelete = () => {
         axios.delete(`https://fe4f5b2b7285d6c0.mokky.dev/advertisements/${ad.id}`)
@@ -33,12 +47,49 @@ function Ad({ ad, onDelete, user }) {
             });
     };
 
+    const handleAddToFavorites = () => {
+        axios.post(`https://fe4f5b2b7285d6c0.mokky.dev/favorites`, {
+            adId: ad.id,
+            userId: currentUser.id
+        })
+        .then(response => {
+            console.log('Объявление успешно добавлено в избранное');
+            setIsFavorite(true);
+            setFavoriteId(response.data.id);
+        })
+        .catch(error => {
+            console.error('Ошибка при добавлении объявления в избранное:', error);
+        });
+    };
+
+    const handleRemoveFromFavorites = () => {
+        axios.delete(`https://fe4f5b2b7285d6c0.mokky.dev/favorites/${favoriteId}`)
+            .then(response => {
+                console.log('Объявление успешно удалено из избранного');
+                setIsFavorite(false);
+                setFavoriteId(null);
+                removeFromFavorites(favoriteId);
+            })
+            .catch(error => {
+                console.error('Ошибка при удалении объявления из избранного:', error);
+            });
+    };
+
     return (
         <div className="Ad">
             <h3 className='Ad-title'>{ad.title}</h3>
             <p className='Ad-price'>Цена: ${ad.price}</p>
             {userName && <p className='Ad-user'>Создано пользователем: {userName}</p>}
-            <button className='Ad-button__delete' onClick={handleDelete}>Удалить</button>
+            {isCurrentUserAd && <button className='Ad-button__delete' onClick={handleDelete}>Удалить</button>}
+            {currentUser && !isCurrentUserAd && (
+                <>
+                    {!isFavorite ? (
+                        <button className='Ad-button__favorite' onClick={handleAddToFavorites}>Добавить в избранное</button>
+                    ) : (
+                        <button className='Ad-button__favorite' onClick={handleRemoveFromFavorites}>Удалить из избранного</button>
+                    )}
+                </>
+            )}
         </div>
     );
 }
--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -25,37 +25,77 @@ function Profile() {
         fetchAllUsers();
     }, []);
 
+    useEffect(() => {
+        // Загрузка избранных объявлений текущего пользователя
+        const fetchFavoriteAds = async () => {
+            try {
+                const response = await axios.get(`https://fe4f5b2b7285d6c0.mokky.dev/favorites?userId=${currentUser.id}`);
+                const favoriteAdsData = [];
+                for (const favorite of response.data) {
+                    const adResponse = await axios.get(`https://fe4f5b2b7285d6c0.mokky.dev/advertisements/${favorite.adId}`);
+                    favoriteAdsData.push({
+                        id: favorite.id,
+                        ad: adResponse.data
+                    });
+                }
+                setFavoriteAds(favoriteAdsData);
+            } catch (error) {
+                console.error('Ошибка при загрузке избранных объявлений:', error);
+            }
+        };
+        if (currentUser) {
+            fetchFavoriteAds();
+        }
+    }, [currentUser]);
+
     const filteredUsers = users.filter(user =>
         user.Name.toLowerCase().includes(searchTerm.toLowerCase())
     );
+
+    const removeFromFavorites = async (favoriteId) => {
+        try {
+            await axios.delete(`https://fe4f5b2b7285d6c0.mokky.dev/favorites/${favoriteId}`);
+            setFavoriteAds(prevFavorites => prevFavorites.filter(favorite => favorite.id !== favoriteId));
+        } catch (error) {
+            console.error('Ошибка при удалении объявления из избранного:', error);
+        }
+    };
 
     return (
         <div className="Profile">
             <Header />
             <header className="Profile-header">
                 <h1 className="Profile-title">Пользователи</h1>
-                {currentUser && <p className='Profile-current-user'>Авторизованный пользователь: {currentUser.Name}</p>}
-                <input
-                    type="text"
-                    placeholder="Поиск..."
-                    className='Profile-input'
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
-                <ul className="Profile-user-list">
-                    <h3 className='Profile-title__h3'>Список пользователей</h3>
-                    {filteredUsers.map(user => (
-                        <li key={user.id} className="Profile-user">{user.Name}</li>
-                    ))}
-                </ul>
+                <div className='Profile-user-block'>
+                    {currentUser && <p className='Profile-current-user'>Авторизованный пользователь: {currentUser.Name}</p>}
+                </div>
+                <div className='Profile-search-box'>
+                    <ul className="Profile-user-list">
+                        <h3 className='Profile-title__h3'>Список пользователей</h3>
+                        <input
+                        type="text"
+                        placeholder="Поиск..."
+                        className='Profile-input'
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                        {filteredUsers.map(user => (
+                            <li key={user.id} className="Profile-user">{user.Name}</li>
+                        ))}
+                    </ul>
+                    
+                </div>
                 <div className="Profile-favorites">
                     <h3 className='Profile-title__h3'>Избранные объявления</h3>
-                    {favoriteAds.map(ad => (
-                        <div key={ad.id} className="Profile-favorite-ad">
-                            <h4>{ad.title}</h4>
-                            <p>Цена: ${ad.price}</p>
-                        </div>
-                    ))}
+                    <div className='Profile-ads'>
+                        {favoriteAds.map(favorite => (
+                            <div key={favorite.id} className="Profile-favorite-ad">
+                                <h4 className='Ad-title'>{favorite.ad.title}</h4>
+                                <p className='Ad-price'>Цена: ${favorite.ad.price}</p>
+                                <button className='Ad-button__delete' onClick={() => removeFromFavorites(favorite.id)}>Удалить из избранного</button>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </header>
         </div>
